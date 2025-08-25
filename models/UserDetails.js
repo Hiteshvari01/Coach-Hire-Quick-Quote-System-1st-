c477d95b4d9c37e4ff6f6cf8d6d030664dd65c98
Hiteshvari01 <hiteshvari5544@gmail.com>
2025-08-25 14:29:14 -0700

    small change

--- models/UserDetails.js
+++ models/UserDetails.js
@@ -21,12 +21,7 @@ const userDetailsSchema = new mongoose.Schema({
     trim: true,
   lowercase: true
   },
-  password: {
-    type: String,
-    required: true,
-    trim: true,
-  lowercase: true
-  },
+  
   additionalInfo: {
     type: String,
     default: "",
